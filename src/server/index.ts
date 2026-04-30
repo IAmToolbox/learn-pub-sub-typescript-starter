0c328b81181125a8250e76319c6a8cabfa55318d
Toolbox <danielalejandro.leivafunes9@gmail.com>
2026-04-29 23:38:33 -0500

    Got started with the basic RabbitMQ connections, and enabled STOMP

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,16 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Connection successful");
+
+  process.on("SIGINT", () => {
+    console.log("\nShutting down program");
+    conn.close();
+    process.exit();
+  });
 }
 
 main().catch((err) => {
